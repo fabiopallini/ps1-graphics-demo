6d72aee7b11811947e976f6957621295c1ea1023
Fabio Pallini <fabiopallini01@gmail.com>
2024-10-15 00:29:33 +0200

    Battle logic moved to ui.c

--- src/ui.h
+++ src/ui.h
@@ -5,6 +5,15 @@
 #include "utils.h"
 #include "enemy.h"
 
+#define MAX_TARGETS 10
+#define FONT_MAX_CHARS 78
+#define SELECTOR_POSY 185
+
+typedef struct {
+	DR_MODE dr_mode[FONT_MAX_CHARS];
+	SPRT sprt[FONT_MAX_CHARS];
+} Font;
+
 typedef struct {
 	Sprite sprite;
 	char prev_display, display;
@@ -12,8 +21,40 @@ typedef struct {
 } Balloon;
 Balloon balloon;
 
+typedef struct {
+	Sprite sprite[4];
+	int display_time;
+} DMG;
+
+typedef struct {
+	float value;
+	Sprite bar, border; 
+} ATB;
+
+typedef struct Battle {
+	ATB atb[2];
+	Sprite command_bg, selector;
+	u_char command_mode;
+	u_char command_index;
+	u_char command_attack;
+
+	u_char target;
+	u_char target_counter;
+	u_char targets[MAX_TARGETS];
+	u_char calc_targets;
+
+	Character chars[3];
+	Enemy enemies[3];
+	DMG dmg;
+} Battle;
+
 void font_init(Font *font);
 void init_balloon(Balloon *b, u_short tpage, int screen_w, int screen_h);
 void set_balloon(Balloon *b, char *text);
+void battle_init(Battle *battle, u_short tpage, int screenW, int screenH);
+void reset_battle_targets(Battle *battle);
+void display_dmg(DMG *dmg, VECTOR pos, int h, int damage);
+void openBattleMenu(Battle *battle);
+void closeBattleMenu(Battle *battle);
 
 #endif
--- src/utils.h
+++ src/utils.h
@@ -7,10 +7,6 @@
 #include "enemy.h"
 #include "char.h"
 
-#define MAX_TARGETS 10
-#define FONT_MAX_CHARS 78
-#define SELECTOR_POSY 185
-
 typedef struct Node {
     void *data;
     struct Node *next;
@@ -77,38 +73,6 @@ typedef struct Background {
 	u_short tpages[2];
 } Background;
 
-typedef struct {
-	Sprite sprite[4];
-	int display_time;
-} DMG;
-
-typedef struct {
-	DR_MODE dr_mode[FONT_MAX_CHARS];
-	SPRT sprt[FONT_MAX_CHARS];
-} Font;
-
-typedef struct {
-	float value;
-	Sprite bar, border; 
-} ATB;
-
-typedef struct Battle {
-	ATB atb[2];
-	Sprite command_bg, selector;
-	u_char command_mode;
-	u_char command_index;
-	u_char command_attack;
-
-	u_char target;
-	u_char target_counter;
-	u_char targets[MAX_TARGETS];
-	u_char calc_targets;
-
-	Character chars[3];
-	Enemy enemies[3];
-	DMG dmg;
-} Battle;
-
 void node_push(Node **node, void *data);
 void node_free(Node **node);
 int inCameraView(Sprite s, long cameraX);
@@ -119,10 +83,5 @@ size_t strlen_delimiter(const u_char *ptr, u_char delimiter);
 void print_bytes(u_long *buffer, size_t size);
 void background_init(Background *b);
 void background_draw(Background *b, long otz, void(*draw)(Sprite *sprite, long otz));
-void battle_init(Battle *battle, u_short tpage, int screenW, int screenH);
-void reset_battle_targets(Battle *battle);
-void display_dmg(DMG *dmg, VECTOR pos, int h, int damage);
-void openBattleMenu(Battle *battle);
-void closeBattleMenu(Battle *battle);
 
 #endif
